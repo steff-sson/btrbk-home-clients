f7058f11bb984f0cc84248aa8e5944e68a5bd0ea
stefan <stefan@weitzelnet.com>
2024-12-29 15:38:02 +0100

    Aktualisierung

--- scripts/btrbk.sh
+++ scripts/btrbk.sh
@@ -1,14 +1,19 @@
-#!/usr/bin/sudo bash
+#!/bin/bash
 #
-# main script caller
+## Ask for sudo priviledges
+echo "$(whoami)"
+[ "$UID" -eq 0 ] || exec sudo "$0" "$@"
+#
+## main script
 read -p "Snapshots und Backups starten? [J]a, [N]ein" -n 1 -r
 echo # neue Zeile
 if [[ $REPLY =~ ^[Jj]$ ]]
 then
   echo "Starte btrbk für Snapshots und Backups!"
   /usr/bin/btrbk -c /etc/btrbk/root.conf run
-  # /usr/bin/btrbk -c /etc/btrbk/home.conf run # optional command for further run of configs
+  /usr/bin/btrbk -c /etc/btrbk/home.conf run # optional command for further run of configs
   echo "Backups und Snapshots sind fertig."
+  read -p "Drücke eine beliebige Taste zum Beenden." -n 1
 else
   echo "Breche ab."
 fi
