a713278cb219574b9671ac79bf3694ab9f752d4d
stefan <stefan@weitzelnet.com>
2025-01-06 08:02:47 +0100

    Reihenfolge der Befehle geändert.

--- scripts/btrbk.sh
+++ scripts/btrbk.sh
@@ -3,14 +3,15 @@
 ## main script
 read -p "Snapshots und Backups starten? [J]a, [N]ein" -n 1 -r
 echo # neue Zeile
-## Ask for sudo priviledges
-echo "$(whoami)"
-[ "$UID" -eq 0 ] || exec sudo "$0" "$@"
 #
 if [[ $REPLY =~ ^[Jj]$ ]]
 then
   echo "Speichere installierte Pakete nach $HOME/pkglist.txt"
-  pacman -Q|cut -f 1 -d " " >> $HOME/pkglist.txt
+  echo # neue Zeile
+  pacman -Q|cut -f 1 -d " " >> $HOME/pkglist.txt  
+  ## Ask for sudo priviledges
+  echo "Du bist als $(whoami) angemeldet."
+  [ "$UID" -eq 0 ] || exec sudo "$0" "$@"
   echo "Starte btrbk für Snapshots und Backups!"
   /usr/bin/btrbk -c /etc/btrbk/root.conf run
   /usr/bin/btrbk -c /etc/btrbk/home.conf run # optional command for further run of configs
